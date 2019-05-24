8522a29e2f0507a3a1de071d4f95884b5594e910
Ayush Gupta <46589053+ayushgupta9198@users.noreply.github.com>
2019-05-24 19:25:24 +0530

    Update House_prediction.py

--- House_prediction.py
+++ House_prediction.py
@@ -1,22 +1,29 @@
-import numpy as np
+import numpy as np #import packages 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_validate
-desired_width = 400
+desired_width = 400 # change the display width in Pycharm console
 pd.set_option('display.width',desired_width)
 pd.set_option('display.max_column',30)
-df=pd.read_csv("C:\\Users\\Ayush gupta\\Downloads\\kc_house_data.csv")
-df1 = pd.DataFrame(df)
+
+#Convert the file into CSV format and import it from local system
+
+df=pd.read_csv("C:\\Users\\Ayush gupta\\Downloads\\kc_house_data.csv") 
+df1 = pd.DataFrame(df) # converted into dataframe from series
+
 cols=['id','date','price','bedrooms','sqft_living','sqft_lot',
-      'floors','sqft_above','sqft_basement','yr_built','zipcode','lat','long','sqft_living15','sqft_lot15']
+      'floors','sqft_above','sqft_basement','yr_built','zipcode','lat','long','sqft_living15','sqft_lot15'] # Filtered coloumns from data
+
 train1=df.drop(['id','price'],axis=1)
-print(df[:].isnull().sum())
-print(train1.head())
-print(df.describe())
-df['bedrooms'].value_counts().plot(kind='pie')
+print(df[:].isnull().sum()) # Convert the null values into valid values
+
+# print(train1.head()) # Display the top results on Pycharm Console
+# print(df.describe()) # Display the describe function
+
+df['bedrooms'].value_counts().plot(kind='pie') # For data visualization through Matplotlib in Pie bar
 plt.title('Number of bedrooms')
 k = [7,2,12,1]
 labels = 'Hall','Kitchen','Living_room','Bedroom','Bathroom','Store_room'
@@ -25,16 +32,21 @@ plt.xlabel('Bedrooms')
 plt.ylabel('count')
 sns.despine()
 # plt.legend()
-plt.show()
-reg=LinearRegression()
+# plt.show() # Display the pie chart
+
+reg=LinearRegression() # Apply linear regression on data
+
 conv_dates=[1 if values == 2014 else 0 for values in df.date ]
-df['date']=conv_dates
+df['date']=conv_dates #Convert the date format into a single valid number
+
 labels=df['price']
 train1=df.drop(['id','price'],axis=1)
-x_train, x_test, y_train, y_test = train_test_split(train1, labels, test_size=0.10,random_state=2)
+x_train, x_test, y_train, y_test = train_test_split(train1, labels, test_size=0.10,random_state=2) # Split the data set into train case and test case.
+
+# Applied algorithm
 reg.fit(x_train,y_train)
-predictions = reg.predict(x_test)
+predictions = reg.predict(x_test) # Predict the result
 print(predictions)
-pred=reg.score(x_test,y_test)*100
-print(pred)
+pred=reg.score(x_test,y_test)*100 # Predict the regression score
+print(pred) 
 
